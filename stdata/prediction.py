d38c402ca53726428795c75c1998b57fba6ab6c0
Oliver Hamelijnck <ohamelijnck@gmail.com>
2023-01-23 20:34:42 +0000

    update

--- stdata/prediction.py
+++ stdata/prediction.py
@@ -66,3 +66,88 @@ def batch_predict(XS, prediction_fn=None, batch_size=1000, verbose=False, axis=0
 
         return y_mean, y_var
 
+def slice_array(A, arr):
+    _A = []
+    for a in arr:
+        _A.append(A[a, :])
+    return np.vstack(_A)
+
+def slice_array_insert(A,B, arr):
+    for i, a in enumerate(arr):
+        _A_len = A[a].shape[0]
+
+        A[a] = A[a] + B[i*_A_len:(i+1)*_A_len, :]
+
+    return A
+
+
+def st_batch_predict(model , XS, prediction_fn=None, batch_size=5000, verbose=False):
+    """
+        With KF models the prediction data must be at all timesteps. This function
+            breaks up the space-time data into space-time batches (which spans across all time points)
+    """
+    #sort XS into grid/'kronecker' structure
+
+    XS = np.roll(XS, -1, axis=1)
+    #sort by time points
+    grid_idx = np.lexsort(XS.T)
+    #reset time axis
+    XS = np.roll(XS, 1, axis=1)
+
+    inv_grid_idx = np.argsort(grid_idx)
+
+    XS = XS[grid_idx]
+
+
+    time_points = np.unique(XS[:, 0])
+    num_time_points = time_points.shape[0]
+    num_spatial_points = int(XS.shape[0]/num_time_points)
+
+    #number of spatial points that fit into batch
+
+    #num_spatial_points_per_batch = int(np.floor(batch_size/num_time_points))
+    num_spatial_points_per_batch = batch_size
+
+    num_steps = max(1, int(np.floor(num_spatial_points/num_spatial_points_per_batch)))
+
+    if verbose:
+        print('num_time_points: ', num_time_points)
+        print('num_spatial_points: ', num_spatial_points)
+        print('num_steps: ', num_steps)
+        print('num_spatial_points_per_batch: ', num_spatial_points_per_batch)
+
+    #empty prediction data
+    mean = np.zeros([XS.shape[0], 1])
+    var = np.zeros_like(mean)
+
+    for i in trange(num_steps):
+
+        batch = num_spatial_points_per_batch
+        if i == num_steps-1:
+            #select the remaining spatial points
+            batch = num_spatial_points - i*num_spatial_points_per_batch
+
+        #k*num_spatial_points is the index to the j'th time slice
+        #i*num_spatial_points_per_batch is index to current spatial batch
+        start_idx = lambda j: j*num_spatial_points + i*num_spatial_points_per_batch
+        end_idx = lambda j: start_idx(j) + batch
+
+        step_idx = [
+            slice(start_idx(j), end_idx(j)) for j in range(num_time_points)
+        ]
+
+        _XS = slice_array(XS, step_idx)
+
+        if prediction_fn is not None:
+            _mean, _var = prediction_fn(_XS)
+        else:
+            _mean, _var = model.predict_y(_XS, diagonal_var=True)
+
+        _mean = np.squeeze(_mean).reshape([-1, 1])
+        _var = np.squeeze(_var).reshape([-1, 1])
+
+        mean = slice_array_insert(mean, _mean, step_idx)
+        var = slice_array_insert(var, _var, step_idx)
+
+    #unsort grid/kronecker structre
+    return [mean[inv_grid_idx]], [var[inv_grid_idx]]
